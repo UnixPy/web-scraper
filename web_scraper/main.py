f4c686e8c2b63c5437553d5b537a2604cf45bdd5
Linus <flovildmidtlielinus@gmail.com>
2020-12-10 21:37:02 +0100

    some general fixes

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -11,17 +11,22 @@ price = web.scrape_web(source=web_link, parser='html.parser')
 
 today = datetime.date.today()
 
-sender = "flovildmidtlielinus@gmail.com"
-reciever = "kjetil@flovild-midtlie.name"
+
+sender = "flovildmidtlielinus@gmail.com" # my gmail
+reciever = os.environ.get('KETIL_GMAIL')
 
 token = os.environ.get('GMAIL_APP_CODE') # password for gmail account
 
+
 subject = "Web Scraper Status!"
+
 price_lower = f"This message was sent using an automated python module. The date of which this gmail was sent is {today}. The price of the rtx 3070 is: {price} The price of the rtx 3070 has dropped since the last gmail! If the rtx 3070 is still on sale the next day, another gmail will be recieved.\n\nsincerely WEB_BOT()"
 
 price_same = f"This message was sent using an automated python module. The date of which this gmail was sent is {today}. The current price of the rtx 3070 is: {price} The price has not changed since last gmail.\n\nIf the price of the rtx 3070 stays the same until monday, another gmail will be recieved.\n\nsincerely WEB_BOT()"
 
+
 while True:
+
     # Scrapes website for price
     web_link = requests.get('https://www.komplett.no/product/1168752/datautstyr/pc-komponenter/skjermkort/asus-geforce-rtx-3070-dual').text
     price = web.scrape_web(source=web_link, parser='html.parser')
@@ -40,5 +45,3 @@ while True:
         sleep(86400)
     else:
         sleep(3600)
-
-
