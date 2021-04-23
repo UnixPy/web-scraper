88337933fab2540edc28c91b187213746e60b72e
Linus <flovildmidtlielinus@gmail.com>
2021-04-23 18:52:59 +0200

    changed stuff

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -32,13 +32,17 @@ while True:
 
     print(price)
 
+
     if price < 5690:
+        print('baba boey1')
         gmail.send_gmail(sender, reciever, token, subject, price_lower)
         gmail.send_gmail(sender, sender, token, subject, price_lower)
         sleep(86400)
-    elif price == 5690 and datetime.date.today().weekday() == 0:
+    elif price == 5690:
+        print('baba boey2')
         gmail.send_gmail(sender, reciever, token, subject, price_same)
         gmail.send_gmail(sender, sender, token, subject, price_same)
         sleep(86400)
     else:
+        print('baba boey3')
         sleep(3600)
